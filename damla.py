a526853cbcf0d05a0ad30cd6f6f12031fc718f01
TheDarkArchive <79809102+TheDarkArchive@users.noreply.github.com>
2021-03-28 01:45:31 +0300

    damla.py

---
+++ damla.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+
+os.system("clear")
+os.system("termux-setup-storage")
+os.system("pkg install git")
+os.system("pkg install curl")
+os.system("pkg install wget")
+os.system("pkg install python")
+os.system("pkg install python2")
+os.system("pkg install python3")
+os.system("pkg install php")
+os.system("pkg install perl")
+os.system("pkg install nano")
+os.system("pkg install vim")
+os.system("pkg install cat")
+os.system("pkg install pip")
+os.system("pkg install pip2")
+os.system("pip install wordlist")
+os.system("pkg install mmap")
+os.system("pkg install hydra")
+os.system("pkg install openssl")
+os.system("pkg install nodejs")
+os.system("pkg install cowsay")
+os.system("clear")
+os.system("cowsay KURULUM BITTI DAMLACİK")
+
+
+
+
+
+
+
+print(“TERMUX KULANIMA HAZİR ABDULLAH SAYGİLAR DİLER MCK ")
